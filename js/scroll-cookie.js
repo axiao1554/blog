e7fd4279a2820197ff404c5288c9af41cd1b493c
axiao1554 <397480224@qq.com>
2021-06-26 19:35:48 +0800

    Site updated: 2021-06-26 19:35:07

--- js/scroll-cookie.js
+++ js/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 26 2021 13:33:26 GMT+0800 (GMT+08:00)
+// build time:Sat Jun 26 2021 19:34:31 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
